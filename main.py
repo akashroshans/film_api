b90548768cc036183ce59300887f8d257d90ddc8
Akash Roshan <akashroshan248@gmail.com>
2025-04-14 13:25:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from flask_cors import CORS
 import joblib
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load your ML model
 model = joblib.load("film_model.pkl")
-CORS(app)
+
+# Define request schema
 class FilmFeatures(BaseModel):
     R: int
     G: int
     B: int
 
+# Define prediction endpoint
 @app.post("/predict")
 def predict(features: FilmFeatures):
     X = [[features.R, features.G, features.B]]
